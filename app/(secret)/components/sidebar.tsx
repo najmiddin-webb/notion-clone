5d68c727913d9798adedeebe720a8b711d02890c
Najmiddin <turgunpolatov>
2024-03-25 06:52:15 +0500

    client

--- app/(secret)/components/sidebar.tsx
+++ app/(secret)/components/sidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { cn } from "@/lib/utils";
 import { ChevronsLeft, ChevronsRight, MenuIcon } from "lucide-react";
 import React, { ElementRef, useEffect, useRef, useState } from "react";
