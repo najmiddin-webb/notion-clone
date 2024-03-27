ad2c1df1d7c3521816008dbf76460026e92f7a3e
Najmiddin <turgunpolatov>
2024-03-27 07:52:59 +0500

    useConvexAuth error fixed

--- app/(secret)/layout.tsx
+++ app/(secret)/layout.tsx
@@ -12,7 +12,7 @@ const SecretLayout = ({ children }: ChildProps) => {
 
   if (isLoading) {
     return (
-      <div className="w-full h-full flex items-center justify-center">
+      <div className="w-full min-h-screen flex items-center justify-center">
         <Loader />
       </div>
     );
